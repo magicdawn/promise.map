b32e73866522fb93eff881b399ba150004a4dc2f
magicdawn <magicdawn@qq.com>
2022-11-13 18:42:29 +0800

    feat: prevent start new tash after rejected

--- src/index.js
+++ src/index.js
@@ -15,8 +15,28 @@ module.exports = function pmap(arr, fn, concurrency) {
     var started = 0
     var running = 0
     var results = new Array(arr.length)
+    var rejected = false
 
-    ;(function replenish() {
+    function start(index) {
+      var cur = arr[index]
+      Promise.resolve(fn.call(cur, cur, index, arr))
+        .then(function (result) {
+          running--
+          completed++
+          results[index] = result
+
+          replenish()
+        })
+        .catch(function (err) {
+          rejected = true
+          reject(err)
+        })
+    }
+
+    function replenish() {
+      // if any previous item rejected, do not start others
+      if (rejected) return
+
       if (completed >= arr.length) {
         return resolve(results)
       }
@@ -24,20 +44,11 @@ module.exports = function pmap(arr, fn, concurrency) {
       while (running < concurrency && started < arr.length) {
         running++
         started++
-        ;(function (index) {
-          var cur = arr[index]
-          Promise.resolve(fn.call(cur, cur, index, arr))
-            .then(function (result) {
-              running--
-              completed++
-              results[index] = result
-
-              replenish()
-            })
-            .catch(reject)
-        })(started - 1)
+        start(started - 1)
       }
-    })()
+    }
+
+    replenish()
   })
 }
 
--- test/simple.js
+++ test/simple.js
@@ -1,13 +1,13 @@
 require('should')
 const _ = require('lodash')
 const chance = require('chance')
-const map = require('../')
+const pmap = require('../')
 
 describe('It works', function () {
   it('simple API', async function () {
     let arr = _.range(5) // [0 .. 4]
 
-    arr = await map(arr, function (x) {
+    arr = await pmap(arr, function (x) {
       return new Promise(function (resolve, reject) {
         setTimeout(function () {
           resolve(x * 2)
@@ -25,7 +25,7 @@ describe('It works', function () {
 describe('Error should be reported', function () {
   it('concurrency not number', function () {
     try {
-      map(
+      pmap(
         [1, 2, 3],
         function (x) {
           return Promise.resolve(x)
@@ -38,5 +38,40 @@ describe('Error should be reported', function () {
       e.should.instanceOf(TypeError)
       e.message.should.match(/is not a number/)
     }
+  })
+})
+
+describe('Feature', function () {
+  it('should not start new after errored', async () => {
+    const arr = [1, 2, 3, 4, 5, 6]
+    const called = []
+
+    try {
+      await pmap(
+        arr,
+        (val) => {
+          return new Promise((resolve, reject) => {
+            setTimeout(function () {
+              if (val === 3) {
+                return reject(new Error('boom when val = 3'))
+              }
+
+              called.push(val)
+              resolve(val)
+            }, 10) // (10ms * val) cost per item
+          })
+        },
+        2
+      )
+    } catch (e) {
+      e.message.includes('boom').should.be.ok()
+    }
+
+    called.includes(1).should.ok()
+    called.includes(2).should.ok()
+    called.includes(3).should.not.ok()
+    called.includes(4).should.not.ok()
+    called.includes(5).should.not.ok()
+    called.includes(6).should.not.ok()
   })
 })
