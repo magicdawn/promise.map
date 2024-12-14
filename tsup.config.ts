69c60798607ed425cc2d9bcc06ec555a66e32ff9
magicdawn <magicdawn@qq.com>
2024-12-14 17:09:38 +0800

    chore: tweak tsup config

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'tsup'
 export default defineConfig((options) => {
   return {
     entry: ['src/index.ts'],
-    format: 'esm',
+    format: ['esm', 'cjs'],
     target: 'node16',
     clean: true,
     esbuildOptions(options, context) {
