c5ae9d07a00443ff0c3eafea2d2dafcb0a4013e5
magicdawn <magicdawn@qq.com>
2024-12-18 13:58:30 +0800

    test: fix approximateCostTime in ci

--- test/_shared.ts
+++ test/_shared.ts
@@ -1,5 +1,6 @@
 export function approximateCostTime(time: number, tolerance: number) {
   return function satisfy(val: number) {
-    return val >= time && val - time <= Math.abs(tolerance)
+    // GitHub Actions 会出现比预期还小的 time, 差 0.5ms, 这里给 1ms 误差
+    return (val >= time || (process.env.CI && val >= time - 1)) && val - time <= Math.abs(tolerance)
   }
 }
